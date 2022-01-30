a34b453a7db58e7d9a22cdc25dddb121e91095a2
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2022-01-30 19:23:55 +0530

    Time: 41 ms (5.15%), Space: 73.2 MB (30.57%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> unique = new HashSet<Integer>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            unique.add(nums[i]);
+        }
+        
+        if(unique.size()== nums.length)
+            return false;
+        return true;
+        
+    }
+}
